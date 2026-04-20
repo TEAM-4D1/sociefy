bcf75a875b96c76048803323073cfba8e1207b76
Trips424 <uditini431@gmail.com>
2026-04-20 17:23:21 +0100

    Registration: Added start registration screen and identified what needs to be added

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
+import '../theme/colours.dart';
+import 'package:provider/provider.dart';
+import '../providers/app_state.dart';
 
-class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({Key? key}) : super(key: key);
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({Key? key}) : super(key: key);
+
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Register')),
-      body: const Center(child: Text('Registration form goes here')),
-    );
-  }
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
 }
+
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _upNumberController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _emailController.dispose();
+    _upNumberController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
