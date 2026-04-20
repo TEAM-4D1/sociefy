732658e29db47161a053abd53f8e3455afe3024f
waynedsz <waynedesouza33@gmail.com>
2026-04-20 14:37:40 +0100

    Implement dynamic list of societies in SocietiesScreen using Consumer for state management

--- lib/screens/societies_screen.dart
+++ lib/screens/societies_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/app_state.dart';
 
 class SocietiesScreen extends StatelessWidget {
   const SocietiesScreen({Key? key}) : super(key: key);
@@ -7,8 +9,20 @@ class SocietiesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Societies')),
-      body: const Center(
-        child: Text('Loading...'),
+      body: Consumer<AppState>(
+        builder: (context, appState, _) {
+          final societies = appState.societies;
+          return ListView.builder(
+            itemCount: societies.length,
+            itemBuilder: (context, index) {
+              final society = societies[index];
+              return ListTile(
+                title: Text(society.name),
+                subtitle: Text(society.category),
+              );
+            },
+          );
+        },
       ),
     );
   }
