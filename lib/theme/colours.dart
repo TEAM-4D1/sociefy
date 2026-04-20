7df1888bce7627d8bc9441ef23108be18e4fa66a
waynedsz <waynedesouza33@gmail.com>
2026-04-20 14:04:46 +0100

    Add AppColours class for centralized color management

---
+++ lib/theme/colours.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColours {
+  static const Color primaryPurple = Color(0xFF4A148C);
+  static const Color accentAmber = Color(0xFFFFC107);
+  static const Color backgroundLight = Color(0xFFF5F5F5);
+  static const Color cardWhite = Color(0xFFFFFFFF);
+  static const Color textDark = Color(0xFF212121);
+  static const Color textGrey = Color(0xFF757575);
+  static const Color successGreen = Color(0xFF43A047);
+}
