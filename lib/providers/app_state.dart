6c95e3ed9f69031e071b1856bcbfe2624a7aedbc
waynedsz <waynedesouza33@gmail.com>
2026-04-20 14:16:16 +0100

    Implement AppState class to manage societies and events with join, leave, save, and unsave functionalities

---
+++ lib/providers/app_state.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/foundation.dart';
+import '../models/society.dart';
+import '../models/event.dart';
+import '../data/sample_societies.dart';
+import '../data/sample_events.dart';
+
+class AppState extends ChangeNotifier {
+  List<Society> _societies = List.from(sampleSocieties);
+  List<Event> _events = List.from(sampleEvents);
+  List<String> _savedEventIds = [];
+
+  List<Society> get societies => List.unmodifiable(_societies);
+
+  List<Society> get joinedSocieties =>
+      _societies.where((s) => s.isJoined).toList();
+
+  List<Society> get availableSocieties =>
+      _societies.where((s) => !s.isJoined).toList();
+
+  List<Event> get events => List.unmodifiable(_events);
+
+  List<Event> get savedEvents =>
+      _events.where((e) => _savedEventIds.contains(e.id)).toList();
+
+  List<Event> eventsForSociety(String societyId) =>
+      _events.where((e) => e.societyId == societyId).toList();
+
+  void joinSociety(String id) {
+    final idx = _societies.indexWhere((s) => s.id == id);
+    if (idx != -1 && !_societies[idx].isJoined) {
+      _societies[idx] = _societies[idx].copyWith(isJoined: true);
+      notifyListeners();
+    }
+  }
+
+  void leaveSociety(String id) {
+    final idx = _societies.indexWhere((s) => s.id == id);
+    if (idx != -1 && _societies[idx].isJoined) {
+      _societies[idx] = _societies[idx].copyWith(isJoined: false);
+      notifyListeners();
+    }
+  }
+
+  void saveEvent(String id) {
+    if (!_savedEventIds.contains(id)) {
+      _savedEventIds.add(id);
+      notifyListeners();
+    }
+  }
+
+  void unsaveEvent(String id) {
+    if (_savedEventIds.remove(id)) {
+      notifyListeners();
+    }
+  }
+}
