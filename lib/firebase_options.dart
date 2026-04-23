84dc8d1fb5c7500909686bb355d52e73bfe6e323
waynedsz <waynedesouza33@gmail.com>
2026-04-23 17:10:31 +0100

    Add stub implementation for FirebaseOptions in firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,1 +1,11 @@
-// TODO Implement this library.
+// Stub for FirebaseOptions to allow compilation. Replace with real config for production.
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform => const FirebaseOptions(
+    apiKey: 'stub',
+    appId: 'stub',
+    messagingSenderId: 'stub',
+    projectId: 'stub',
+  );
+}
