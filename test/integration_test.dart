0d467b435e24e3c7d9deda03e84cd851bfd1ea24
chikbunting <chikbunting04@gmail.com>
2026-04-23 16:29:47 +0100

    emulate the website

--- test/integration_test.dart
+++ test/integration_test.dart
@@ -1,16 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:sociefy/main.dart' as app;
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('Performance Tests', () {
     testWidgets('Messages tab loads within 2 seconds', (tester) async {
       final stopwatch = Stopwatch()..start();
 
       // Start the app
-      app.main();
+      binding.attachRootWidget(MaterialApp(home: app.MyApp()));
       await tester.pumpAndSettle();
 
       // Navigate to Messages tab
@@ -26,7 +27,7 @@ void main() {
       final stopwatch = Stopwatch()..start();
 
       // Start the app
-      app.main();
+      binding.attachRootWidget(MaterialApp(home: app.MyApp()));
       await tester.pumpAndSettle();
 
       // Simulate joining a society
@@ -42,7 +43,7 @@ void main() {
   group('Usability Tests', () {
     testWidgets('UI is accessible', (tester) async {
       // Start the app
-      app.main();
+      binding.attachRootWidget(MaterialApp(home: app.MyApp()));
       await tester.pumpAndSettle();
 
       // Check for accessibility labels
