2756ba9da4ea9c030b3472bb3d6d167eaba4ada9
waynedsz <waynedesouza33@gmail.com>
2026-05-06 21:12:07 +0100

    refactor: remove unused import for sign_in_screen in register_screen.dart

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sociefy/screens/sign_in_screen.dart';
 import '../services/auth_service.dart';
 
 /// Allows new students to create an account with display name, email, and password.
