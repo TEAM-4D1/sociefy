1488e2c1e42c49b4b9ef943af1f6a3f41e9f84ee
Justin Burfoot <jburfoot12@gmail.com>
2026-05-08 20:06:45 +0100

    update AppBarLogoAction size and remove shadow decoration

--- lib/widgets/app_bar_logo_action.dart
+++ lib/widgets/app_bar_logo_action.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppBarLogoAction extends StatelessWidget {
-  const AppBarLogoAction({super.key, this.size = 44});
+  const AppBarLogoAction({super.key, this.size = 70});
 
   final double size;
 
@@ -13,15 +13,6 @@ class AppBarLogoAction extends StatelessWidget {
         child: Container(
           width: size,
           height: size,
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withValues(alpha: 0.14),
-                blurRadius: 6,
-                offset: const Offset(0, 1),
-              ),
-            ],
-          ),
           child: Image.asset(
             'assets/setap logo for vs code.png',
             fit: BoxFit.contain,
