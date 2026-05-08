b14cde8a7f9826e91afaf80dd23732285e777d33
Trips424 <uditini431@gmail.com>
2026-05-08 19:59:04 +0100

    update AppBarLogoAction padding and size properties

--- lib/widgets/app_bar_logo_action.dart
+++ lib/widgets/app_bar_logo_action.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 
 class AppBarLogoAction extends StatelessWidget {
-  const AppBarLogoAction({super.key, this.size = 40});
+  const AppBarLogoAction({super.key, this.size = 44});
 
   final double size;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(right: 12),
+      padding: const EdgeInsets.only(right: 10),
       child: Center(
-        child: SizedBox(
+        child: Container(
           width: size,
           height: size,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withValues(alpha: 0.14),
+                blurRadius: 6,
+                offset: const Offset(0, 1),
+              ),
+            ],
+          ),
           child: Image.asset(
             'assets/setap logo for vs code.png',
             fit: BoxFit.contain,
